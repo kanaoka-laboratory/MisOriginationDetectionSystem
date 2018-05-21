4083f69fa4ed811b290ff66cbe4bfae12bdefd8f
Hironori Imai <6518002i@st.toho-u.ac.jp>
2018-05-22 01:21:33 +0900

    衝突を検出して標準出力に出すとこまで

--- import/analysis.php
+++ import/analysis.php
@@ -36,4 +36,23 @@ function getFullRouteFromBgpdump($filename, &$network_list){
 	return true;
 }
 
+function detectConflict(&$network_list1, &$network_list2){
+	// network_list1のASとnetwork_list2のASを総当りで比較（ただし同じAS同士はスキップ）
+	foreach($network_list1['v4'] as $as1 => $prefix_list1){
+		foreach($network_list2['v4'] as $as2 => $prefix_list2){
+			// 2つのASがそれぞれ広告しているIPプレフィックス同士を比較
+			foreach($prefix_list1 as $prefix1){
+				foreach($prefix_list2 as $prefix2){
+					// 衝突するプレフィックスがあれば出力
+					if(detectIpv4PrefixOverlap($prefix1, $prefix2)){
+						echo "AS$as1:$prefix1<=>AS$as2:$prefix2\n";
+					}
+
+				}
+			}		
+
+		}
+	}
+}
+
 ?>
